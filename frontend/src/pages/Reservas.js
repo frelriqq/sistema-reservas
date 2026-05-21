2617a7d4b55fd742bb0012dece9546a7d2ab96d6
frelriqq <andrespajaro133@gmail.com>
2026-05-21 11:29:47 -0500

    corrige función para verificar disponibilidad en el módulo de reservas

--- frontend/src/pages/Reservas.js
+++ frontend/src/pages/Reservas.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { crearReserva, misReservas, cancelarReserva } from '../services/api';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { crearReserva, misReservas, cancelarReserva, verificarDisponibilidad } from '../services/api';
