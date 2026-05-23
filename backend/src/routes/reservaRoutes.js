9efbacee7db2096badcdc2f263ed37a9392cab69
frelriqq <andrespajaro133@gmail.com>
2026-05-22 19:36:46 -0500

    agrega ruta para eliminar reservas en el módulo de reservas

--- backend/src/routes/reservaRoutes.js
+++ backend/src/routes/reservaRoutes.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const { verificarToken, soloAdmin } = require('../middleware/auth');
-const { crearReserva, misReservas, cancelarReserva, todasLasReservas, cambiarEstado, verificarDisponibilidad } = require('../controllers/reservaController');
+const { crearReserva, misReservas, cancelarReserva, todasLasReservas, cambiarEstado, verificarDisponibilidad, eliminarReserva } = require('../controllers/reservaController');
 router.post('/', verificarToken, crearReserva);
 router.get('/mis-reservas', verificarToken, misReservas);
 router.put('/cancelar/:id', verificarToken, cancelarReserva);
 router.get('/todas', verificarToken, soloAdmin, todasLasReservas);
 router.put('/estado/:id', verificarToken, soloAdmin, cambiarEstado);
 router.get('/disponibilidad', verificarToken, verificarDisponibilidad); 
+router.delete('/:id', verificarToken, soloAdmin, eliminarReserva);
 
 module.exports = router;
